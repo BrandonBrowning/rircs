5ca39744067e790d19a7f84aeb5d88d6c9c9fd8a
BrandonBrowning <brandonbrowning@live.com>
2016-02-12 22:03:10 -0500

    Added pong response function and test, and maybe integrated it

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,19 @@ use std::io::{BufRead, Write};
 use std::net::{TcpStream};
 use bufstream::BufStream;
 
+fn ping_response(line: &str) -> Option<String> {
+    let pieces = line.split(' ').collect::<Vec<_>>();
+    if pieces.len() > 1 {
+        let command = pieces[0];
+        if command == "PING" {
+            let response = format!("PONG {}", pieces[1]);
+            return Some(response);
+        }
+    }
+
+    None
+}
+
 fn io() -> io::Result<()> {
     let raw_stream = try!(TcpStream::connect("irc.freenode.net:6667"));
     let mut stream = BufStream::new(raw_stream);
@@ -28,6 +41,13 @@ fn io() -> io::Result<()> {
 
         println!("< {}", line);
 
+        if let Some(response) = ping_response(&line) {
+            try!(stream.write(response.as_bytes()));
+            try!(stream.flush());
+
+            println!("> {}", response);
+        }
+
         line.clear();
     }
 
@@ -39,4 +59,9 @@ fn main() {
         Ok(()) => (),
         Err(e) => println!("Error: {}", e.description()),
     }
+}
+
+#[test]
+fn test_ping_response() {
+    assert_eq!(ping_response("PING :foo"), Some("PONG :foo".to_string()));
 }
